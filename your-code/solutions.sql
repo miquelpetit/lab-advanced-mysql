f79ee6d58a965f90a271cf8d21c8f4ea5ebcc03b
Miquel <mpetitsierra@gmail.com>
2019-07-15 18:30:38 +0200

    adding the steps for the first challenge

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,4 +1,18 @@
 -- Challenge 1
+-- step 1
+select  titleauthor.title_id, titleauthor.au_id, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as royalties
+from titleauthor
+join sales on sales.`title_id` = titleauthor.`title_id`
+join titles on titles.`title_id` = titleauthor.`title_id` ;
+
+-- step 2
+select  titleauthor.title_id, titleauthor.au_id, sum(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as total_royalties
+from titleauthor
+join sales on sales.`title_id` = titleauthor.`title_id`
+join titles on titles.`title_id` = titleauthor.`title_id`
+group by titleauthor.`au_id`, titleauthor.`title_id`;
+
+-- step 3
 
 select titleauthor.`au_id`, 
 titleauthor.`title_id`, 
